5bac966596c3630a17739256e688f6e7bdfd2264
Zamir <Zamir@DIVISs-MacBook-Air.local>
2019-09-22 16:53:23 -0700

    First Commit

---
+++ CollectData2.java
@@ -0,0 +1,165 @@
+     
+import java.util.Arrays;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+
+/** Class that calculates runtime prints them to file. 
+   *Authors: Greg Maxwell and Zamir Thind
+   *Programming Assignment 2
+   *CPSC-3273-AO1 
+   */
+
+public class CollectData2 {
+   
+  /** Main driver for class. 
+   *@param args not used.
+   *@throws IOException for print.
+   */
+   public static void main(String[] args) 
+      throws IOException {
+      
+      // Declare, allocate array of 100000 ints, a[0]...a[999999]
+      int[] huge = new int[1000000];       
+      
+      //Creates new file and printwriter.
+      FileWriter fw = new FileWriter("output.txt");
+      PrintWriter pw = new PrintWriter(fw);
+      pw.println("In order of appearance: n, T(n)/log2(n), T(n)/nlog2(n), and T(n)/(n^2)log2(n)"); 
+   
+      //Assigns random values to each element.
+      for (int i = 0; i < huge.length; i++) {
+         huge[i] = (int) (Math.random() * Integer.MAX_VALUE);  // Random number 0-999999
+      }
+      
+      // Run and plot sortArray in increments of 1000
+      for (int n = 10; n <= huge.length; n += 1000) { 
+         
+         double myN = (double) n;
+         // Declare, allocate array of n ints
+         int[] newArray = new int[n];          
+         
+         // Copy n values from array huge into new array
+         newArray = Arrays.copyOf(huge, n);       
+         
+         // Get time prior to sorting
+         long startTime = System.currentTimeMillis();          
+         
+         // Call sort method to sort new array
+         // sortArray(newArray);
+         mergeSort(newArray, 0, newArray.length - 1); 
+      
+         // Get time after sorting
+         long endTime = System.currentTimeMillis(); 
+         
+         // Subtract endTime - startTime for elapsed time
+         long elapsedTime = (endTime - startTime);
+         
+         // T(elapsedTime)/log2(n)
+         double tOflog2 = elapsedTime / (log2(myN));
+         
+         // T(elapsedTime)/log2(n)
+         double tOflog2TimesN = elapsedTime / (myN * (log2(myN)));
+         
+         // T(elapsedTime)/log2(n)
+         double tOflog2TimesNSquared = elapsedTime / (nSquared(myN) * (log2(myN)));
+         
+         // Prints length of new arrays        
+         System.out.println("For n ints: " + newArray.length);
+         
+         // Prints time elapsed  
+         System.out.println("Elapsed time: " + elapsedTime);
+         
+         // Prints T(n)/log2(n) 
+         System.out.println("T(n)/log2(n): " + tOflog2);
+         
+         // Prints T(n)/log2(n) 
+         System.out.println("T(n)/nlog2(n): " + tOflog2TimesN);
+         
+         // Prints T(n)/log2(n) 
+         System.out.println("T(n)/(n^2)log2(n): " + tOflog2TimesNSquared);
+              
+         // Write n, T(n)/log2(n), T(n)/nlog2(n), and T(n)/(n^2)log2(n) to output.txt
+         pw.println(n + ", " + tOflog2 + ", " + tOflog2TimesN + ", " + tOflog2TimesNSquared); 
+              
+      }
+      
+      pw.close();
+   }
+   
+   public static void merge(int A[], int p, int q, int r) {
+   
+   // Find sizes of two subarrays to merge
+      int n1 = q - p + 1;
+      int n2 = r - q;
+      
+   // Create temp arrays
+      int L[] = new int[n1 + 1];
+      int R[] = new int[n2 + 1];
+      
+      for (int i = 0; i < n1; i++)
+         L[i] = A[p + i];
+      for (int j = 0; j < n2; j++)
+         R[j] = A[q + 1 + j];
+         
+      L[n1] = Integer.MAX_VALUE;
+      R[n2] = Integer.MAX_VALUE;
+   
+   // Initialize indexes of first and second subarrays
+      int i = 0;
+      int j = 0;
+   
+   // Initial index of merged subarray
+      for (int k = p; k <= r; k++) {
+         if (L[i] <= R[j]) {
+            A[k] = L[i];
+            i = i + 1;
+         }
+         else {
+            A[k] = R[j];
+            j = j + 1;
+         }
+      }
+   }
+
+   public static void mergeSort(int A[], int p, int r) {
+      if (p < r) {
+         int q = ((p + r) / 2);
+      
+         mergeSort(A, p, q);
+         mergeSort(A, q + 1, r);
+      
+         merge(A, p, q, r);
+      }
+   }
+   
+   public static double log2(double n) {
+      double convLog = 0;
+      convLog = (double) (Math.log(n) / Math.log(2));
+      return convLog;
+   }
+   
+   public static double nSquared(double n) {
+      double n2 = 0;
+      n2 = (double) Math.pow(n, 2);
+      return n2;
+   }
+   
+   public static void insertSort(int A[]){
+       int n = A.length;
+       for(int i = 1; i < n; ++i) {
+         int key = A[i];
+         int j = i - 1;
+       
+       /* Move elements of arr[0..i-1], that are 
+          greater than key, to one position ahead 
+          of their current position */
+       while(j >= 0 && A[j] > key) {
+         A[j + 1] = A[j];
+         j = j -1;
+       }
+         A[j + 1] = key;
+      }
+   }
+}
